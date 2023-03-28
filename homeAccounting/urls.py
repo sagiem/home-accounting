579eb00855f22064503956bfa5238adf95cbddec
Maksim Kashin <sagiems@gmail.com>
2023-03-28 15:36:52 +0600

    изменена модель пользователей

--- homeAccounting/urls.py
+++ homeAccounting/urls.py
@@ -4,8 +4,6 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
+    # path('', include('core.urls', namespace='core')),
 ]
