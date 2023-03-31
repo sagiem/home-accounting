6caef417e35e650584bfce4a9547cfde880e0c31
Maksim Kashin <sagiems@gmail.com>
2023-03-31 12:57:21 +0600

    Добавил регистрацию на основе логина и емаил

--- users/serializers.py
+++ users/serializers.py
@@ -5,13 +5,27 @@ from users import models
 
 class RegisteUser(serializers.Serializer):
     username = serializers.CharField()
+    email = serializers.EmailField()
     password = serializers.CharField(min_length=8)
-
 
     def validate_username(self, value):
         if models.CustomUser.objects.filter(username=value).exists():
             raise serializers.ValidationError('Пользователь с таким именем уже есть')
+        print(value)
         return value
+
+    def validate_email(self, value):
+        if models.CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Пользователь с таким email уже есть')
+        print(value)
+        return value
+
+    def validate(self, attrs):
+        print(attrs)
+        return attrs
+
+
+
 
 class LoginUser(serializers.Serializer):
     username = serializers.CharField()
