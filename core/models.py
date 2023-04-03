84819608805a32edf61d5e6d1afa7a33ccd23831
Maksim Kashin <sagiems@gmail.com>
2023-04-03 16:36:04 +0600

    модели доходов и расходов

--- core/models.py
+++ core/models.py
@@ -4,9 +4,12 @@ from django.db import models
 # Доходы дома
 class Income(models.Model):
     created = models.DateTimeField('Создан', auto_now_add=True, db_index=True)
+    money = models.BigIntegerField('Сумма')
+
 
 
 
 # Расходы дома
 class Expenses(models.Model):
     created = models.DateTimeField('Создан', auto_now_add=True, db_index=True)
+    money = models.BigIntegerField('Сумма')
